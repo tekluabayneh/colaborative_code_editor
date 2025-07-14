21a2538ccde114e308a2218ad91796d2c1177579
teklu abayneh <tekluabayneh@gmail.com>
2025-07-14 23:31:06 +0300

    Add Owners model for new user schema

---
+++ backend/models/Owners.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import type { Owners } from "../types/Owners";
+
+const OwnersSchema = new mongoose.Schema<Owners>({
+    userName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 30,
+    },
+    role: {
+        type: String,
+        enum: ["Owner"],
+        default: "Owner",
+    }
+})
+const Owners = mongoose.model<Owners>("Onwser", OwnersSchema);
+
+export default Owners;
