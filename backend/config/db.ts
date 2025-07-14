a21c2d0af8bf6f5949275eacc686c143064b555a
teklu abayneh <tekluabayneh@gmail.com>
2025-07-14 23:31:05 +0300

    Update DB connection logic or logging in db.ts

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -3,14 +3,17 @@ import mongoose from "mongoose";
 
 const mongoConnect = async () => {
     try {
-        mongoose.connect(process.env.MongoDB_URI!)
+        await mongoose.connect(process.env.MongoDB_URI!)
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
-        process.exit(1); // Exit the process with failure
+        process.exit(1);
     }
 }
 
 export default mongoConnect;
 
+
+// db-password=RqkMJnpAg3bROSRQ
+//mongodb-url=mongodb+srv://tekluabayneh:RqkMJnpAg3bROSRQ@cluster0.h8u2gll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
