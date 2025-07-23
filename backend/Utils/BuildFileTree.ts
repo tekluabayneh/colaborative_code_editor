d658977f336f8069e1ea726582ad4d6143bcb974
tekluabayneh <tekluabayneh@gmail.com>
2025-07-23 11:57:42 +0000

    Add BuildFileTree utility

---
+++ backend/Utils/BuildFileTree.ts
@@ -0,0 +1,20 @@
+
+
+type FileList ={
+  name:string,
+  type:string,
+  _id:string,
+  parentId:number ,
+    }
+
+const BuildTree =(FlatList:FileList , parentId:string) => {
+
+return <FileList>FlatList.filter((item) => String(item.parentId) == String(parentId))
+    .map(item =>(
+     {...item,
+        children: BuildTree(FlatList, item._id)
+        }
+        ))
+}
+
+export default BuildTree
