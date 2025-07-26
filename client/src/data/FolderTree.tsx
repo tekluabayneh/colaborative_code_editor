c7ae8f943ad651a5cdb47dcb9a28b29fc88e5876
tekluabayneh <tekluabayneh@gmail.com>
2025-07-26 22:25:07 +0000

    Add unique ID generation to initial folder structure

--- client/src/data/FolderTree.tsx
+++ client/src/data/FolderTree.tsx
@@ -1,49 +1,71 @@
 import {Node } from "../types/Node"
+const generateId = () => crypto.randomUUID(); 
 export const fileTree: Node[] = [
-  {
-    name: 'Home',
-    nodes: [
-      {
-        name: 'Movies',
+    {
+        
+        folderid:generateId(),
+        name: 'Home',
         nodes: [
-          {
-            name: 'Action',
-            nodes: [
-              {
-                name: '2000s',
+            {
+                folderid:generateId(),
+                name: 'Movies',
                 nodes: [
-                  { name: 'Gladiator.mp4' },
-                  { name: 'The-Dark-Knight.mp4' },
+                    {
+                        folderid:generateId(),
+                        name: 'Action',
+                        nodes: [
+                            {
+                                folderid:generateId(),
+                                name: '2000s',
+                                nodes: [
+                                    { name: 'Gladiator.mp4' },
+                                    { name: 'The-Dark-Knight.mp4' },
+                                ],
+                            },
+                            {
+
+                                folderid:generateId(),
+                                name: '2010s', nodes: [] },
+                        ],
+                    },
+                    {
+                        folderid:generateId(),
+                        name: 'Comedy',
+                        nodes: [{ name: '2000s', nodes: [{ name: 'Superbad.mp4' }] }],
+                    },
+                    {
+                        folderid:generateId(),
+                        name: 'Drama',
+                        nodes: [
+                            {
+                                folderid:generateId(),
+                                name: '2000s', nodes: [{ name: 'American-Beauty.mp4' }] },
+                        ],
+                    },
                 ],
-              },
-              { name: '2010s', nodes: [] },
-            ],
-          },
-          {
-            name: 'Comedy',
-            nodes: [{ name: '2000s', nodes: [{ name: 'Superbad.mp4' }] }],
-          },
-          {
-            name: 'Drama',
-            nodes: [
-              { name: '2000s', nodes: [{ name: 'American-Beauty.mp4' }] },
-            ],
-          },
+            },
+            {
+                folderid:generateId(),
+                name: 'Music',
+                nodes: [
+                    {
+                        folderid:generateId(),
+                        name: 'Rock', nodes: [] },
+                    {
+                        folderid:generateId(),
+                        name: 'Classical', nodes: [] },
+                ],
+            },
+            { 
+
+                folderid:generateId(),
+                name: 'Pictures', nodes: [] },
+            {
+                folderid:generateId(),
+                name: 'Documents',
+                nodes: [],
+            },
+            { name: 'passwords.txt' },
         ],
-      },
-      {
-        name: 'Music',
-        nodes: [
-          { name: 'Rock', nodes: [] },
-          { name: 'Classical', nodes: [] },
-        ],
-      },
-      { name: 'Pictures', nodes: [] },
-      {
-        name: 'Documents',
-        nodes: [],
-      },
-      { name: 'passwords.txt' },
-    ],
-  },
+    },
 ];
