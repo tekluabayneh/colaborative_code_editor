580ed02f472ec09fb0360fd47827a54b928cdd1f
teklu abayneh <tekluabayneh@gmail.com>
2025-07-26 20:32:07 +0300

    Update form.cy.ts: normalize line endings and apply code changes

--- E2E/cypress/e2e/Frontend/form.cy.ts
+++ E2E/cypress/e2e/Frontend/form.cy.ts
@@ -1,11 +1,31 @@
 /// <reference types="cypress" />
 
+
 describe("form test", () => {
   it("it should register", () => {
     cy.visit("http://localhost:3000");
     cy.contains("Sign in").click();
+    cy.get(`[data-testid="DontHave_Account"]`).click() 
 
-    cy.get(`[data-testid="testid_email"]`).type("");
-    cy.get(`[data-testid="testid_password"]`).type("");
+
+    cy.get(`[data-testid="testid_username"]`).type("Jone");
+    cy.get(`[data-testid="testid_email"]`).type("example@gmail.com");
+    cy.get(`[data-testid="testid_password"]`).type("this work ma");
+
+   cy.get(`[data-testid="Register_button"]`).click()
+
   });
+
+
+  it("it should login", () => {
+     cy.visit("http://localhost:3000");
+     cy.contains("Sign in").click();
+    cy.get(`[data-testid="testid_email_login"]`).type("example@gmail.com");
+    cy.get(`[data-testid="testid_password_login"]`).type("this work ma");
+
+    cy.get(`[data-testid="testid_login_button"]`).click() 
+
+
+
+});
 });
