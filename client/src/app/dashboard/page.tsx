d44fdd6a82fd0daecc236d6944baf3d05859f1c5
tekluabayneh <tekluabayneh@gmail.com>
2025-07-26 22:25:06 +0000

    Update dashboard logic for persistent ID support

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -6,20 +6,18 @@ import CodeEditor from  "../../components/Editor"
 import FileSystem from "@/components/FileSystem";
 import { fileTree } from "@/data/FolderTree";
 import {Node } from "../../types/Node"
+import NameInputModal from "../../components/ui/CreateFolderModal" 
 const File= fileTree
 const Home = () => {
     const [resizeFileTree, setReziseFileTree] = useState<number>(42)
     const [getFolderName, setgetFolderName] = useState<string>()
     const [fileTree, setFileTree] = useState(File);
+    const [isModabolen, setisModalopen] = useState(false)
+    const [isFile, setisFile] = useState(false)
 
-    console.log(fileTree) 
 
     const Createfolder = ()=> {
-        const newFolder:Node = {
-            name: "getFolderName",
-            nodes: [{ name: "one" }, { name: "two" }],
-        }; console.log(fileTree) 
-        setFileTree({...fileTree, ...newFolder})
+        setisModalopen(true)
     }
 
     return (
@@ -28,8 +26,12 @@ const Home = () => {
             {/* this is the folder and file tree  */}
             <div className={`h-screen w-40`}>
                 <div className="w-full flex items-center justify-baseline pl-6 gap-9 py-2 border-b-orange-50">
-                    <FolderPlus onClick={() => Createfolder()}  className="w-6 h-6 text-yellow-600 cursor-pointer" />
-                    <FilePlus className="w-6 h-6 text-blue-600 cursor-pointer" />
+                    <FolderPlus onClick={() => { 
+                        setisFile(false)
+                        Createfolder()}}  className="w-6 h-6 text-yellow-600 cursor-pointer" />
+                    <FilePlus    onClick={() => {
+                        setisFile(true)
+                        Createfolder()}}  className="w-6 h-6 text-blue-600 cursor-pointer" />
                 </div>
                 <FileSystem folders={fileTree} />
             </div>
@@ -38,6 +40,10 @@ const Home = () => {
             {/* this is the editor in the middle   */}
             <div className="w-full h-screen grid-cols-3 bg-green-500"> 
                 <CodeEditor/>
+             { 
+                    isModabolen &&
+                   <NameInputModal  setisModalopen={setisModalopen} setFileTree={setFileTree} isFile={isFile}/> 
+                }
             </div>
 
 
