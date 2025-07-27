3281642e3a13eb4d93e1e657217006c6f4e7ec41
tekluabayneh <tekluabayneh@gmail.com>
2025-07-27 20:12:25 +0000

    Update global layout structure in app

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css"
+import {FileSystemProvider } from "../context/FileTreeContext"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,8 +27,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <FileSystemProvider>
         {children}
+        </FileSystemProvider>
       </body>
     </html>
+
+
   );
 }
