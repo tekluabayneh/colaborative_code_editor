3f8110616d326e27d36b9ca3756f341b0df5678d
tekluabayneh <tekluabayneh@gmail.com>
2025-07-28 15:01:36 +0000

    Update Node type definitions

--- client/src/types/Node.ts
+++ client/src/types/Node.ts
@@ -4,4 +4,22 @@ export type Node = {
   nodes?: Node[];
 };
 
+export type FileSystemContextType = {
+    name: string;
+    setName: (value: string) => void;
+    isModabolen: boolean;
+    setisModalopen: (value: boolean) => void;
+    isFile: boolean;
+    setisFile: (value: boolean) => void;
+    fileTree: Node[];
+    setFileTree: React.Dispatch<React.SetStateAction<Node[]>>;
+    handleSubmit: () => void;
+    handleCancel: () => void;
+    Createfolder : () => void;
+    CreateFile_and_FolderWithin:(isFile:boolean) => void
+    Dletefile_and_folder: () => void
+};
+
+
+
 
