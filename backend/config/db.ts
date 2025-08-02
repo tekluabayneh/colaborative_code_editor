af55cbcf88a03fa368f6ca2a1366ac07c3d7bae1
teklu abayneh <tekluabayneh@gmail.com>
2025-08-02 18:27:50 +0300

    chore: modify DB configuration settings

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -20,24 +20,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const mongoConnect = async () => {
-    const MONGODB_URL = process.env.MONGODB_URL;
-    console.log("MONGODB_URL:", MONGODB_URL);
-    if (!MONGODB_URL) {
-        throw new Error("MONGODB_URL environment variable is not set");
-    }
+  // const MONGODB_URL = process.env.MONGODB_URL;
+  const MONGODB_URL = "mongodb://localhost:27017/colab_db";
+  console.log("MONGODB_URL:", MONGODB_URL);
+  if (!MONGODB_URL) {
+    throw new Error("MONGODB_URL environment variable is not set");
+  }
 
-    try {
-        console.log("Attempting to connect to MongoDB...");
-        await mongoose.connect(MONGODB_URL, {
-            retryWrites: true,
-            maxPoolSize: 10,
-            connectTimeoutMS: 10000,
-        });
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        throw error; // Let server.ts handle the error
-    }
+  try {
+    console.log("Attempting to connect to MongoDB...");
+    await mongoose.connect(MONGODB_URL, {
+      retryWrites: true,
+      maxPoolSize: 10,
+      connectTimeoutMS: 10000,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error; // Let server.ts handle the error
+  }
 };
 
 export default mongoConnect;
