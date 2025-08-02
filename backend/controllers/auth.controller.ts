c12e8baff5e1c946538b3763b7d1facdd6c724f1
teklu abayneh <tekluabayneh@gmail.com>
2025-08-02 18:27:50 +0300

    fix: update auth controller logic

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,25 +1,51 @@
 import { Request, Response } from 'express';
 import Tokens from '../Utils/token';
+import checkRole from "../middlewares/role.middleware";
 import Owners from '../models/Owners';
-import { Sign } from 'crypto';
+import HashPassword from '../Utils/hash';
+import validator from "../Utils/validator"
 const Register = async (req: Request, res: Response) => {
-    const { email, password, userName } = req.body;
+	const { email, password, userName } = req.body;
 
-    const userInfo = { userName: userName, password: password, email: email }
-    const AddOwner = await Owners.insertOne(userInfo)
+	const HashedPassword = await HashPassword(password)
 
-    if (!AddOwner || !AddOwner._id) {
-        res.status(400).json({ error: 'User was not registered correctly' });
-        return;
-    }
+	const userInfo = { userName: userName, password: HashedPassword, email: email }
 
-    const token = Tokens.SignUser_JWT_Token(email, process.env.JWT_SECRET_KEY!)
-    res.status(200).json({ message: 'Registration successful', JTw_toke: token });
+	const AddOwner = await Owners.insertOne(userInfo)
+
+	if (!AddOwner || !AddOwner._id) {
+		res.status(400).json({ error: 'User was not registered correctly' });
+		return;
+	}
+
+
+	res.status(200).json({ message: 'Registration successful', });
 }
 
 
 const Login = async (req: Request, res: Response) => {
-    const { email, password, userName } = req.body;
+	const { email, password, userName } = req.body;
+
+	// check usr role get the role from the db using the email
+	const UserRole = await checkRole(email); 
+
+	const HashedPassword = await Owners.findOne({email:email})
+
+	if(HashedPassword?.password){
+		if(!await validator.isPasswordMatch(password, HashedPassword.password)){
+			res.status(404).json({Message:"pasword is not correct"})
+			return
+		}
+
+	}
+
+
+
+
+	const token = Tokens.SignUser_JWT_Token(email, process.env.JWT_SECRET_KEY!)
+	res.status(200).json({message:"user login successful", userRole:UserRole, toen:token})
+
+
 }
 
 
