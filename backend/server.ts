76d9e71b8a75fdfb26eba04f3c791e16a40342d7
teklu abayneh <tekluabayneh@gmail.com>
2025-08-02 18:27:53 +0300

    chore: update backend server logic

--- backend/server.ts
+++ backend/server.ts
@@ -1,21 +1,18 @@
-import mongoConnect from "./config/db"
+import mongoConnect from "./config/db";
 import app from "./app";
 const PORT = process.env.PORT || 5000;
 const startServer = async () => {
-    try {
-        // await mongoConnect();
-        console.log("✅ MongoDB connected");
+  try {
+    await mongoConnect();
+    console.log("✅ MongoDB connected");
 
-        app.listen(PORT, () => {
-            console.log(`🚀 Server is running on http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error("❌ Failed to start server:", error);
-        process.exit(1);
-    }
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to start server:", error);
+    process.exit(1);
+  }
 };
 
 startServer();
-
-
-
