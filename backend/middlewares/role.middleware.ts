40c7db17a3857d2eec51fcddcb63bfba3edbe08d
teklu abayneh <tekluabayneh@gmail.com>
2025-08-02 18:27:51 +0300

    fix: refine role-based access middleware

--- backend/middlewares/role.middleware.ts
+++ backend/middlewares/role.middleware.ts
@@ -1,7 +1,9 @@
 
+import validator from "../Utils/validator"
+const {isUserRoleOwnerOrUser} = validator
 
-const checkRole = (role: string) => {
-
+const checkRole = async (email: string) => {
+       return await isUserRoleOwnerOrUser(email)
 }
 
 
