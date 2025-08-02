1d4d53ca125b211f09a7b0827a7783594f963948
teklu abayneh <tekluabayneh@gmail.com>
2025-08-02 18:27:49 +0300

    fix: enhance validation for user input

--- backend/Utils/validator.ts
+++ backend/Utils/validator.ts
@@ -1,82 +1,105 @@
 import jwt from "jsonwebtoken";
-import { Request } from 'express';
-import type { returnMessage } from "../types/user"
+import { compareSync } from "bcryptjs";
+import { Request } from "express";
+import type { returnMessage } from "../types/user";
 import Owners from "../models/Owners";
-
+import Users from "../models/user";
 const validateEmailAndPassword = (password: string, email: string): boolean => {
-    if (password.length < 6 || password.length > 30) {
-        return false
-    }
-    // if email is not the correct email type return message for the user  
-    const regx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-    return regx.test(email)
-}
+  if (password.length < 6 || password.length > 30) {
+    return false;
+  }
+  // if email is not the correct email type return message for the user
+  const regx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regx.test(email);
+};
 
 const ValidateRegister = (request: Request): returnMessage => {
-    const { email, userName, password } = request.body
-    if (!email || !userName || !password) {
-        return {
-            isValid: false,
-            message: " Validation failed: Missing required fields"
-        }
-    }
+  const { email, userName, password } = request.body;
 
-    if (!validateEmailAndPassword(password, email)) {
-        return {
-            isValid: false,
-            message: "Invalid email or password length is above 30"
-        }
-    }
+
+  if (!email || !userName || !password) {
     return {
-        isValid: true,
-        message: "Validation pass"
-    }
-}
+      isValid: false,
+      message: " Validation failed: Missing required fields",
+    };
+  }
+
+  if (!validateEmailAndPassword(password, email)) {
+    return {
+      isValid: false,
+      message: "Invalid email or password length is above 30",
+    };
+  }
+  return {
+    isValid: true,
+    message: "Validation pass",
+  };
+};
 
 const ValidateLogin = (request: Request): returnMessage => {
-    const { email, password } = request.body
-    if (!email || !password) {
-        return {
-            isValid: false,
-            message: " Validation failed: Missing required fields"
-        }
-    }
-
-    if (!validateEmailAndPassword(password, email)) {
-        return {
-            isValid: false,
-            message: "Invalid email or password length is not correct"
-        }
-
-    }
+  const { email, password } = request.body;
+  if (!email || !password) {
     return {
-        isValid: true,
-        message: "Validation pass"
-    }
-}
+      isValid: false,
+      message: " Validation failed: Missing required fields",
+    };
+  }
 
-const VerifyJwtToken = (token: string, secretKey: string): string | jwt.JwtPayload => {
-    try {
-        const decoded = jwt.verify(token, secretKey)
-        return decoded as jwt.JwtPayload;
-    } catch (error) {
-        return "Invalid token"
-    }
-}
+  if (!validateEmailAndPassword(password, email)) {
+    return {
+      isValid: false,
+      message: "Invalid email or password length is not correct",
+    };
+  }
+  return {
+    isValid: true,
+    message: "Validation pass",
+  };
+};
+
+const VerifyJwtToken = ( token: string, secretKey: string): string | jwt.JwtPayload => {
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    return decoded as jwt.JwtPayload;
+  } catch (error) {
+    return "Invalid token";
+  }
+};
 
 const isUserAlreadyRegistered = async (email: string): Promise<boolean> => {
-    const findUserByEmail = await Owners.find({ email: email })
-    return findUserByEmail.length === 0 ? false : true
+  const findUserByEmail = await Owners.find({ email: email });
+  return findUserByEmail.length === 0 ? false : true;
+};
+
+
+const isUserRoleOwnerOrUser =async(email:string) => {
+const Owners_user = await Owners.findOne({email:email})
+const users_user =  await Users.findOne({email:email})
+
+	// is the result of owners is empty we know its user
+	if(Owners_user){
+         return { role:Owners_user.role, isOwner:true }
+
+	}else if(users_user){
+	  return { role:users_user.role, isOwner:false }
+	}
+
 }
 
-export default { ValidateLogin, ValidateRegister, VerifyJwtToken, isUserAlreadyRegistered }
+const isPasswordMatch = async(password:string, hasedapssword:string): Promise<boolean> => {
+   return compareSync(password,hasedapssword) 
+}
 
 
-// validateing step we hvae to take 
+export default {
+  ValidateLogin,
+  ValidateRegister,
+  VerifyJwtToken,
+  isUserAlreadyRegistered,
+  isUserRoleOwnerOrUser ,
+isPasswordMatch
+};
+
+// validateing step we hvae to take
 // first check if the input value are present or not
 // then check if the input value are valid or not
-
-
-
-
-
