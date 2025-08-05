c438688bb9867587a63f95e5f5af2cfc8022b590
teklu abayneh <tekluabayneh@gmail.com>
2025-08-06 01:41:51 +0300

    feat: enhance auth middleware with improved token handling

--- backend/middlewares/auth.middleware.ts
+++ backend/middlewares/auth.middleware.ts
@@ -1,12 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import validateAuthRequest from "../Utils/validator";
 
-// give them role if they are admin or user
-// check if the user is already registered
 const ResgisterValidate = async ( req: Request, res: Response, next: NextFunction) => {
   const { email, password, userName } = req.body;
 
-  // validate the request body for registration
   if (!validateAuthRequest.ValidateRegister(req).isValid) {
     res
       .status(400)
@@ -26,14 +23,13 @@ const ResgisterValidate = async ( req: Request, res: Response, next: NextFunctio
 const LoginValidate = async ( req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
-  // validate the request body for registration
   if (!validateAuthRequest.ValidateLogin(req)) {
     res.status(400).json({ error: "Invalid registration data" });
     return;
   }
 
   if (!await validateAuthRequest.isUserAlreadyRegistered(email)) {
-    res .status(400) .json({ message: "user does not exit please check you email " });
+    res .status(400) .json({ message: "user does not exists please check you email " });
     return;
   }
 
