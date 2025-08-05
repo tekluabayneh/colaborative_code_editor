36bc9d98267c7e1401a49d4930b67e16025a6e42
teklu abayneh <tekluabayneh@gmail.com>
2025-08-06 01:41:53 +0300

    feat: add OTP model schema

---
+++ backend/models/Otp.ts
@@ -0,0 +1,22 @@
+// models/Otp.ts
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IOtp extends Document {
+  email: string;
+  otp: string;
+  expiresAt?: Date;
+}
+
+const otpSchema: Schema = new Schema<IOtp>({
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  expiresAt: { type: Date, required: true},  
+});
+
+//  Automatically delete expired OTPs
+otpSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+const OtpModel = mongoose.model<IOtp>('Otp', otpSchema);
+
+export default OtpModel;
+
