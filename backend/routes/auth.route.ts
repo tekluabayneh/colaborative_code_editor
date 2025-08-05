37e7e77e64c486edcca94c77a08436886e186da4
teklu abayneh <tekluabayneh@gmail.com>
2025-08-06 01:41:53 +0300

    feat: update auth routes

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import authController from "../controllers/auth.controller";
 import AuthMiddleware from "../middlewares/auth.middleware";
-const { Login, Register } = authController;
+const { Login, Register , verifyOtp, SendOTP} = authController;
 const { ResgisterValidate, LoginValidate } = AuthMiddleware;
 const AuthRouter = express.Router();
 
 AuthRouter.post("/auth/login", LoginValidate, Login);
 
 AuthRouter.post("/auth/register", ResgisterValidate, Register);
+
+AuthRouter.post("/auth/sendOtp", SendOTP )
+
+AuthRouter.post("/auth/verifyOtp", verifyOtp)
 
 export default AuthRouter;
