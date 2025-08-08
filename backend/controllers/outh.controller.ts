6ec8eeb36fd8b8c61477bc1d629b9eb704c50b50
teklu abayneh <tekluabayneh@gmail.com>
2025-08-08 14:42:12 +0300

    feat: implement GitHub OAuth login and callback handling

--- backend/controllers/outh.controller.ts
+++ backend/controllers/outh.controller.ts
@@ -2,16 +2,20 @@ import {Strategy as googlesterategy, type Profile} from "passport-google-oauth20
 import {Strategy as githubsterategy  } from "passport-github2"
 import { type PassportStatic } from "passport"
 import owners from "../models/Owners"
-import users from "../models/user"
- 
+
+interface usertype {
+	userName:string,
+	 email:string,
+	 photo:string 
+}
 
 const GetEmail = (profile:Profile):string => {
- return  profile.emails  && profile.emails?.length > 0 && profile.emails[0].value ? profile.emails[0].value : "";
+	return  profile.emails  && profile.emails?.length > 0 && profile.emails[0].value ? profile.emails[0].value : "";
 }
 
 
 const GetPhoto = (profile: Profile): string => {
-  return profile.photos?.[0]?.value ?? "";
+	return profile.photos?.[0]?.value ?? "";
 };
 
 
@@ -20,43 +24,35 @@ export const configuregoogleAuth = (passport:PassportStatic) => {
 
 	passport.use(
 		new googlesterategy( {
-	 clientID:process.env.GOOGLE_CLIENTID ?? "",
-	 clientSecret:process.env.GOOGLE_CLIENTSECRET ?? "",
-     	  callbackURL:process.env.GOOGLE_CALBACKURL, 
+			clientID:process.env.GOOGLE_CLIENTID ?? "",
+			clientSecret:process.env.GOOGLE_CLIENTSECRET ?? "",
+			callbackURL:process.env.GOOGLE_CALBACKURL, 
 		},
 			async (accesstoken:string, refrechtoken:string, profile:Profile,done:(error:any, user:any)=> void ) => { try {
-					const user= {
-						id:profile.id,
-						firstname:profile.name?.familyName,
-						lastname:profile.name?.givenName,
-						email:GetEmail(profile),
-						photo:GetPhoto(profile)
-					}
+                                   const userName = profile.name?.familyName
+                                   if(!userName) return done(null, false) 
 
-				console.log(user)
+				const user:usertype = {
+					userName:userName,
+					email:GetEmail(profile),
+					photo:GetPhoto(profile)
+				}
 
-					// check if user exist in db 
-					const checkqueryfromowner = await owners.find({email:user.email})
-					const checkqueryfromuser = await users.find({email:user.email})
+				console.log(user) 
 
-					if(checkqueryfromuser){
-						done(null, user)
-						return 
-					} else if (checkqueryfromowner){
-						done(null, user)
-						return 
-					}
+				// check if user exist in db 
+				const checkqueryfromowner = await owners.find({email:user.email})
 
-					// otherwise if the user does not exit register/stre the record in db 
-					const userdata = await owners.insertOne(user)
+				console.log(checkqueryfromowner)
 
-					if(userdata){
-						done(null, user)
-						return
-					}
+				if (checkqueryfromowner) return done(null, user)
+
+				const userdata = await owners.insertOne(user)
+
+				if(userdata) return done(null, user)
 
 
-				} catch (error) {
+			} catch (error) {
 					console.log("error while sign in with google")
 					done(error, null)
 				} 
@@ -73,54 +69,32 @@ export const configuregithubstrateg = (passport:PassportStatic) => {
 			clientID: process.env.GITHUB_CLIENTID!,
 			clientSecret: process.env.GITHUB_CLIENTSECRET!,
 			callbackURL:process.env.GITHUB_CLLBACKURL!,
+			scope: ["user:email"]
 		},
 			async (accesstoken:string, refrechtoken:string, profile:Profile,done:(error:any,user:any)=> void ) => {
 				try {
+					let username
+					if(!profile._json.name) return done(null, false)
 
-					let fullname = profile._json.name;
-
-					let firstname = "";
-					let lastname = "";
-
-					if (fullname) {
-						const parts = fullname.trim().split(" ");
-						firstname = parts[0];
-						lastname = parts.slice(1).join(" ") || "";
-					} else {
-						// fallback: username as both first and last name
-						firstname = profile?.username ?? ""
-						lastname = profile?.username ?? "" 
-					}
-
-					const user = {
-						id: profile.id,
-						username: profile.username,
-						photo: profile.photos?.[0]?.value || "",
-						email: profile.emails?.[0]?.value ?? null,
-						firstname,
-						lastname,
+					username = profile._json.name.trim().split(" ")[0]
+					const user:usertype = {
+						userName: username,
+						email:GetEmail(profile),
+						photo: GetPhoto(profile)
 					};
+
+                                console.log(profile)
+
+				   console.log(user)
 
 					// check if the user exist
 					const checkqueryfromowner = await owners.findOne({email:user.email})
-					const checkqueryfromuser =  await users.findOne({email:user.email}) 
 
-					// if the user already exist just no need to store the data
 					if(checkqueryfromowner) return  done(null, user) 
-						
-					if(checkqueryfromuser) return done(null, user)
 
-
-                                          return 
 					const userdata = await owners.insertOne(user)
-                                       if(!userdata){
-						    done(null, null)
-						return 
-					}
-					if(userdata){
-						done(null, user)
-						return
-					}
+					if(!userdata) return  done(null, null)
+					if(userdata) return done(null, user)
 
 				} catch (error) {
 					console.log("error while sign in with github")
