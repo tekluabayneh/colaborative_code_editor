9a1f6b2074e3a9abc65246c17c09312dd06d1333
teklu abayneh <tekluabayneh@gmail.com>
2025-08-08 14:42:12 +0300

    refactor: update Owners schema to support OAuth users and OTP expiry

--- backend/models/Owners.ts
+++ backend/models/Owners.ts
@@ -2,28 +2,31 @@ import mongoose from "mongoose";
 import type { Owners } from "../types/Owners";
 
 const OwnersSchema = new mongoose.Schema<Owners>({
-    userName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-    role: {
-        type: String,
-        enum: ["Owner"],
-        default: "Owner",
-    }
+	userName: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		lowercase: true,
+	},
+	password: {
+		type: String,
+		required: false,
+		minlength: 6,
+	},
+	photo:{
+		type:String,
+	},
+	role: {
+		type: String,
+		enum: ["Owner"],
+		default: "Owner",
+	}
 })
 const Owners = mongoose.model<Owners>("Owners", OwnersSchema);
 
