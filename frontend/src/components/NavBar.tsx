c6f8c91fad77da6a3e0bf6d529cf871d26ee4304
teklu abayneh <tekluabayneh@gmail.com>
2025-08-09 15:53:25 +0300

    style: update NavBar styles

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -9,27 +9,21 @@ return (
           <div className="flex justify-between items-center h-16">
             {/* Logo and Navigation */}
             <div className="flex items-center space-x-8">
-              <div className="text-white font-semibold text-lg">
+              <div className="text-white font-semibold text-2xl">
                 🖤 CodeSync 
               </div>
               <nav className="hidden md:flex space-x-8">
                 <Link href="#" className="text-gray-300 hover:text-white transition-colors">Product</Link>
                 <Link href="#" className="text-gray-300 hover:text-white transition-colors">Resources</Link>
                 <Link href="/DocDashboard" className="text-gray-300 hover:text-white transition-colors">Docs</Link>
-                <Link href="#" className="text-gray-300 hover:text-white transition-colors">Pricing</Link>
               </nav>
             </div>
             
             {/* Right side buttons */}
             <div className="flex items-center space-x-4">
-              <button className="text-gray-300 hover:text-white transition-colors"
-                onClick={() => route.push('/Auth')}
-                        >
-                Sign in
-              </button>
-              <button className="bg-white text-black px-4 py-2 rounded-md hover:bg-gray-100 transition-colors">
-                Book a demo
-              </button>
+
+              <button className="text-gray-300 cursor-pointer border-1 px-6 py-1 rounded border-gray-800 hover:text-white transition-colors"
+                onClick={() => route.push('/Auth')}> Sign in </button>
             </div>
           </div>
         </div>
