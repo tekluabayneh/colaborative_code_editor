a50544d084233132802dcad54a17681f98a12f36
teklu abayneh <tekluabayneh@gmail.com>
2025-08-11 15:06:01 +0300

    Modify app layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css"
 import {FileSystemProvider } from "../context/FileTreeContext"
+import { Toaster } from 'react-hot-toast';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <FileSystemProvider>
         {children}
+		<Toaster/>
         </FileSystemProvider>
       </body>
     </html>
