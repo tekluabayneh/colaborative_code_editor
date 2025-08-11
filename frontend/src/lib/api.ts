002bb8a1b31ae0af2676980d7854399f47604732
teklu abayneh <tekluabayneh@gmail.com>
2025-08-11 15:06:04 +0300

    Modify API library

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+export async function verifyOtp(code: string,email:string) {
+	try {
+         const response = await axios.post("http://localhost:5000/api/auth/verifyOtp",{email:email,Otp:code})
+	  return response
+	} catch (err) {
+		if(axios.isAxiosError(err)){ 
+               toast.error(err.response?.data.message)
+		return 
+		}
+	     console.log(err)		
+	}
+}
+
