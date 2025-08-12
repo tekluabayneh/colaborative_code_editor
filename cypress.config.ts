1d9eaa660e9ac07acc6f6c8aaf63af81536d68e5
teklu abayneh <tekluabayneh@gmail.com>
2025-08-12 14:02:13 +0300

    Update Cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,21 +1,8 @@
-// const { defineConfig } = require("cypress");
-//
-// module.exports = defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// });
-//
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    //specPattern: "E2E/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    //    specPattern: "E2E/cypress/e2e/**/*.{cy.js,cy.jsx,cy.ts,cy.tsx}",
     specPattern: "E2E/cypress/e2e/**/*.{spec,cy}.{js,jsx,ts,tsx}",
-
     supportFile: "E2E/cypress/support/e2e.js",
     fixturesFolder: "E2E/cypress/fixtures",
     videosFolder: "E2E/cypress/videos",
