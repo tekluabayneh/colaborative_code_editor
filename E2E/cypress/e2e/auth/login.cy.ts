5b0a69cf26207aa453879ef76967143385d7719a
teklu abayneh <tekluabayneh@gmail.com>
2025-08-12 14:02:14 +0300

    Add new auth tests

---
+++ E2E/cypress/e2e/auth/login.cy.ts
@@ -0,0 +1,11 @@
+
+	//
+//   it("it should login", () => {
+//      cy.visit("http://localhost:3000");
+//      cy.contains("Sign in").click();
+//     cy.get(`[data-testid="testid_email_login"]`).type("example@gmail.com");
+//     cy.get(`[data-testid="testid_password_login"]`).type("this work ma");
+//
+//     cy.get(`[data-testid="testid_login_button"]`).click() 
+//
+// });
---
+++ E2E/cypress/e2e/auth/register.cy.ts
@@ -0,0 +1,65 @@
+/// <reference types="cypress" />
+
+describe("form test", () => {
+
+  const fillRegisterForm = (username:string,passowrd:string,email:string) => { 
+   		cy.get(`[data-testid="testid_username"]`).clear().type(username)
+		cy.get(`[data-testid="testid_email"]`).clear().type(passowrd)
+		cy.get(`[data-testid="testid_password"]`).clear().type(email)
+	}
+
+
+
+	beforeEach(() => {
+		cy.visit("http://localhost:3000");
+		cy.contains("Sign in").click();
+		cy.get(`[data-testid="DontHave_Account"]`).click() 
+	})
+
+
+	it("should password show error message and button has to be disabled", () => {
+                 fillRegisterForm("fake","fake_email","fake")
+		cy.contains("Password must be at least 6 characters").should("be.visible")
+		cy.get(`[data-testid="Register_button"]`).should("be.disabled")
+	})
+
+	it("should return invalid email error message",() => { 
+
+		cy.get(`[data-testid="Register_button"]`).should("be.disabled")
+
+                 fillRegisterForm("fake","fake_work_ma","fake_email")
+
+		cy.get(`[data-testid="Register_button"]`).should("not.be.disabled")
+
+		cy.get(`[data-testid="Register_button"]`).click() 
+		cy.contains("Invalid Form data", {timeout:2000}).should("be.visible")
+	})
+
+
+
+	it("should return error message for user already registred ", () => {
+                 fillRegisterForm("jone","fake@gmail.com","fake_work_ma")
+
+		cy.get(`[data-testid="Register_button"]`).should("not.be.disabled")
+		cy.get(`[data-testid="Register_button"]`).click()
+
+		cy.contains("user already exist please check you email ").should("be.visible") 
+		cy.url().should("not.include", "/verifyOtp")
+		cy.get(`[data-testid="Register_button"]`).should("be.disabled")
+	})
+
+
+
+	it("it should  get 200 success message ", () => {
+                 fillRegisterForm("jone","fake_2@gmail.com","fake_work_ma")
+
+		cy.get(`[data-testid="Register_button"]`).should("not.be.disabled")
+		cy.get(`[data-testid="Register_button"]`).click()
+
+		cy.contains("Registration successful, now verify Otp",{timeout:1500}).should("be.visible") 
+		cy.url().should("include", "/verifyOtp")
+
+	})
+
+
+})
