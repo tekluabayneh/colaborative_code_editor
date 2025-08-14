572bf12cf4089718a5ea4d5955465bc1521bf18a
teklu abayneh <tekluabayneh@gmail.com>
2025-08-14 12:57:49 +0300

    test: update register.cy.ts for OTP input behavior

--- E2E/cypress/e2e/auth/register.cy.ts
+++ E2E/cypress/e2e/auth/register.cy.ts
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import { forEach } from "cypress/types/lodash"
+
 describe("form test", () => {
 
   const fillRegisterForm = (username:string,passowrd:string,email:string) => { 
@@ -50,20 +52,77 @@ describe("form test", () => {
 
 
 
-	it.only("it should  get 200 success message ", () => {
-                 fillRegisterForm("jone","fake_2@gmail.com","fake_work_ma")
+	it("it should  get 200 success message ", () => {
+                 fillRegisterForm("jone","fake_11@gmail.com","fake_work_ma")
+		cy.get(`[data-testid="Register_button"]`).should("not.be.disabled")
+		cy.get(`[data-testid="Register_button"]`).click()
 
+		cy.contains("Registration successful, now verify Otp",{timeout:1500}).should("be.visible") 
+		cy.url().should("include", "/verifyOtp")
+	})
+})
+
+
+
+// verify otp 
+describe("otp verification", () => { 
+	beforeEach(() => {
+		cy.visit("http://localhost:3000");
+	})
+
+  const fillRegisterForm = (username:string,passowrd:string,email:string) => { 
+   		cy.get(`[data-testid="testid_username"]`).clear().type(username)
+		cy.get(`[data-testid="testid_email"]`).clear().type(passowrd)
+		cy.get(`[data-testid="testid_password"]`).clear().type(email)
+	}
+
+
+ 	it("should return error toster message", () => {
+		cy.contains("Sign in").click();
+		cy.get(`[data-testid="DontHave_Account"]`).click() 
+                 fillRegisterForm("jone","amn@gmail.com","fake_work_ma")
 		cy.get(`[data-testid="Register_button"]`).should("not.be.disabled")
 		cy.get(`[data-testid="Register_button"]`).click()
 
 		cy.contains("Registration successful, now verify Otp",{timeout:1500}).should("be.visible") 
 		cy.url().should("include", "/verifyOtp")
 
+               cy.get(`[data-testid="btn_verify"]`).should("be.disabled")
+
+		const fakeOtp = [1,2,3,4,5,6] 
+		fakeOtp.forEach(num => { 
+                 cy.get(`[data-testid="otp_input"]`).type(`${num}`)
+	       })
+
+               cy.get(`[data-testid="btn_verify"]`).should("not.be.disabled")
+               cy.get(`[data-testid="btn_verify"]`).click()
+                     
+               	cy.contains("no OTP found for this email",{timeout:1500}).should("be.visible")
 	})
 
+ 	it.only("should return success toster message", () => {
+		cy.contains("Sign in").click();
+		cy.get(`[data-testid="DontHave_Account"]`).click() 
+                 fillRegisterForm("jone","fake_16@gmail.com","fake_work_ma")
+		cy.get(`[data-testid="Register_button"]`).should("not.be.disabled")
+		cy.get(`[data-testid="Register_button"]`).click()
 
-	it.only("should throw invalid opt code ", () => { 
-	
+		cy.contains("Registration successful, now verify Otp",{timeout:1500}).should("be.visible") 
+		cy.url().should("include", "/verifyOtp")
+
+               cy.get(`[data-testid="btn_verify"]`).should("be.disabled")
+
+ 		const fakeOtp = ["G","0","-","W","8","e"] 
+		fakeOtp.forEach(num => { 
+                 cy.get(`[data-testid="otp_input"]`).type(`${num}`)
+	       })
+
+               cy.get(`[data-testid="btn_verify"]`).should("not.be.disabled")
+               cy.get(`[data-testid="btn_verify"]`).click()
+                     
+               	cy.contains("OTP verifyed successfully").should("be.visible")
 	})
 
 })
+
+
