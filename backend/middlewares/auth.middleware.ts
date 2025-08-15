d3dc18045623cc76b0c1b73e5c55ae798264fe4b
teklu abayneh <tekluabayneh@gmail.com>
2025-08-15 23:58:15 +0300

    feat: implement authentication middleware with typed user

--- backend/middlewares/auth.middleware.ts
+++ backend/middlewares/auth.middleware.ts
@@ -2,11 +2,12 @@ import { Request, Response, NextFunction } from "express";
 import validateAuthRequest from "../Utils/validator";
 import Users from "../models/user";
 import Owners from "../models/Owners";
+import Jwt, { JwtPayload } from "jsonwebtoken";
 
 const ResgisterValidate = async ( req: Request, res: Response, next: NextFunction) => {
 	if(!req.body.email || !req.body.password || !req.body.userName){ 
 		res.status(400).json({ message: "all input are mandatory" });
-		 return 
+		return 
 	}
 
 	const { email, password, userName } = req.body;
@@ -30,7 +31,7 @@ const ResgisterValidate = async ( req: Request, res: Response, next: NextFunctio
 const LoginValidate = async ( req: Request, res: Response, next: NextFunction) => {
 	if(!req.body.email || !req.body.password){ 
 		res.status(400).json({ message: "all input are mandatory" });
-		 return 
+		return 
 	}
 
 	const { email, password } = req.body;
@@ -50,7 +51,7 @@ const LoginValidate = async ( req: Request, res: Response, next: NextFunction) =
 
 
 const ResetPasswordValidate =  async (req:Request, res:Response, next:NextFunction) => { 
-       const body = req.body || {};
+	const body = req.body || {};
 	if(!body.newPassword || !req.query.token || !req.query.email){ 
 		res.status(400).json({message:"all input are mandatory"})
 		return
@@ -68,7 +69,32 @@ const ResetPasswordValidate =  async (req:Request, res:Response, next:NextFuncti
 	next()
 }
 
+const authenticate = (req:Request, res:Response, next:NextFunction) =>{ 
+	const token = req.cookies.accessToken  
+
+	if(!token || Array.isArray(token)){ 
+		res.status(401).json({ message: "Not authenticated" });
+		return 
+	} 
+
+	try {
+
+		const decoded = Jwt.verify(token, process.env.JWT_SECRET_KEY!) as JwtPayload
+
+		if(typeof decoded == "object" && decoded.email && decoded.role){ 
+			req.user = decoded 
+			next()
+		}else{ 
+			res.status(403).json({ message: "Invalid token payload" }); 
+		}
+
+	} catch (error) {
+		res.status(403).json({ message: "Invalid or expired token" });	
+	} 
+
+}
+
 
 // check if the user is  oging useing invite code and if the invite code is valid and redirect them to the dashboard and set thier role to user or admin or super admin
 
-export default { ResgisterValidate, LoginValidate , ResetPasswordValidate};
+export default { ResgisterValidate, LoginValidate ,authenticate, ResetPasswordValidate};
