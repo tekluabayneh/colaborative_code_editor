c2de184d90bbf4e045325063373ee8189e4568d8
teklu abayneh <tekluabayneh@gmail.com>
2025-08-15 23:58:16 +0300

    feat: enhance email service for invites and reset links

--- backend/services/email.service.ts
+++ backend/services/email.service.ts
@@ -9,6 +9,45 @@ const transport: Transporter = nodemailer.createTransport({
 })
 
 // reset passwordk verification
+export const sendInvitationLink  = (to:string, resetLink:string) => { 
+	const sendInvitationLinkMailOption: SendMailOptions = {
+		from: `"CodeSync" <${process.env.GMAIL}>`,
+		to: to,
+		subject: "Password Reset Request - CodeSync",
+		html: `
+<div style="font-family: Arial, sans-serif; background-color: #f7f8fa; padding: 30px; text-align: center;">
+
+<h1 style="color: #4A90E2; font-size: 28px; margin-bottom: 10px;">
+<span style="background-color:#4A90E2; color:#fff; padding:8px 14px; border-radius:6px;">
+CodeSync
+</span>
+</h1>
+
+<div style="background-color: #ffffff; padding: 25px; border-radius: 8px; max-width: 400px; margin: 0 auto; box-shadow: 0 2px 8px rgba(0,0,0,0.05);">
+<h2 style="color: #333; font-size: 20px;">Reset Your Password</h2>
+<p style="color: #555; font-size: 15px; line-height: 1.5;">
+We received a request to reset your password. Click the button below to set a new password:
+</p>
+<div style="margin: 20px 0;">
+<a href="${resetLink}" 
+style="display: inline-block; background-color: #4A90E2; color: #fff; padding: 12px 20px; font-size: 16px; text-decoration: none; border-radius: 6px;">
+Reset Password
+</a>
+</div>
+<p style="color: #888; font-size: 13px;">
+If you didn’t request this change, you can safely ignore this email. This link will expire in 15 minutes.
+</p>
+</div>
+
+<p style="color: #999; font-size: 12px; margin-top: 20px;">
+&copy; ${new Date().getFullYear()} CodeSync. All rights reserved.
+</p>
+</div>
+`,
+	};
+	transport.sendMail(sendInvitationLinkMailOption)	
+}
+// reset passwordk verification
 export const sendResetPasswordLink = (to:string, resetLink:string) => { 
 	const resetPasswordMailOption: SendMailOptions = {
 		from: `"CodeSync" <${process.env.GMAIL}>`,
@@ -31,7 +70,7 @@ We received a request to reset your password. Click the button below to set a ne
 <div style="margin: 20px 0;">
 <a href="${resetLink}" 
 style="display: inline-block; background-color: #4A90E2; color: #fff; padding: 12px 20px; font-size: 16px; text-decoration: none; border-radius: 6px;">
-Reset Password
+ accept Invitation 
 </a>
 </div>
 <p style="color: #888; font-size: 13px;">
