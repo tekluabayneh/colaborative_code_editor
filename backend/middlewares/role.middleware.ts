22a0084cf3b778860053392edccee997b6f15952
teklu abayneh <tekluabayneh@gmail.com>
2025-08-16 16:03:36 +0300

    fix: role authorization middleware update

--- backend/middlewares/role.middleware.ts
+++ backend/middlewares/role.middleware.ts
@@ -3,18 +3,23 @@ import { Request, Response, NextFunction } from "express";
 
 const authorizeRoles = (...role:string[]) => {
 	return (req:Request ,res:Response, next:NextFunction) => {
-
-		if(req.user && !req.user.role){ 
+		try {
+				if(!req.token){ 
 			res.status(401).json({ message: "Not authenticated" });
 			return
 		}
-
-		if(!role.includes(req.user.role)){
+             console.log("log from authorizeRoles",req.token)
+		if(!role.includes(req.token.role)){
+                  console.log("problem here",req.token)
 			res.status(403).json({ message: "Forbidden: insufficient role" });
 			return
 		}
 
 		next()
+		} catch (error) { 
+		console.log(error)
+		}
+
 	} 
 } 
 
