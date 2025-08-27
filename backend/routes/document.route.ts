f78f9ff85e28d39d73d623dc2cd3176f47371791
teklu abayneh <tekluabayneh@gmail.com>
2025-08-27 11:40:36 +0300

    fix(backend): update document routes handling

--- backend/routes/document.route.ts
+++ backend/routes/document.route.ts
@@ -4,9 +4,15 @@ import  DocController from "../controllers/document.controller"
 const DocumentRouter = Router()
 
 /// get single document when user click file tree
-DocumentRouter.post("/doc/GetSingleDocument:DocId", (req, res) => DocController.GetdocumetnById(req, res))
+DocumentRouter.get("/doc/GetSingleDocument/:DocId", (req, res) => DocController.GetdocumetnById(req, res))
 
 DocumentRouter.get("/doc/GetAllFolderTree", (req, res) => DocController.GetAllFolderTree(req, res))
+
+DocumentRouter.put("/doc/UpdateFolderOrFileName", (req, res) => DocController.UpdateFolder_or_file_name(req, res))
+
+DocumentRouter.delete("/doc/DeleteDocument/:DocId", (req, res) => DocController.DeletedocumentById(req, res))
+
+DocumentRouter.put("/doc/UpdateDocument/:DocId", (req, res) => DocController.UpdatedocumentById(req, res))
 
 
 export default DocumentRouter
