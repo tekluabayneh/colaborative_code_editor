7c2eba24dc16fcb7819e7c40f3ccd5f5fb771462
teklu abayneh <tekluabayneh@gmail.com>
2025-09-01 09:22:24 +0300

    chore: adjust editor layout

--- frontend/src/app/editor/layout.tsx
+++ frontend/src/app/editor/layout.tsx
@@ -1,18 +1,18 @@
-// import type { Metadata } from "next";
-// import {FileContentProvider} from "../context/EditorContext"
-//
-// export const metadata: Metadata = {
-//   title: "editor page",
-//   description: "editor apge",
-// };
-//
-// export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
-//   return (
-//       <div> 
-//         <FileContentProvider>
-// 		{children}
-//         </FileContentProvider>
-//       </div>
-//   );
-// }
-//
+import type { Metadata } from "next";
+import {FileContentProvider} from "../../context/EditorContext"
+
+export const metadata: Metadata = {
+  title: "editor page",
+  description: "editor apge",
+};
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+  return (
+      <div> 
+        <FileContentProvider>
+		{children}
+        </FileContentProvider>
+      </div>
+  );
+}
+
