dd546e19af400b8722e919d26b8fd154ab79856c
teklu abayneh <tekluabayneh@gmail.com>
2025-09-01 16:10:25 +0300

    fix: update document routes handling

--- backend/routes/document.route.ts
+++ backend/routes/document.route.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import  DocController from "../controllers/document.controller"
-
 const DocumentRouter = Router()
+import  authorizeRoles from "../middlewares/role.middleware"   
+import  authenticateMiddleware from "../middlewares/auth.middleware"
+const {authenticate} = authenticateMiddleware 
 
 /// get single document when user click file tree
-DocumentRouter.get("/doc/GetSingleDocument/:DocId", (req, res) => DocController.GetdocumetnById(req, res))
+DocumentRouter.get("/doc/GetSingleDocument/:DocId", authenticate, authorizeRoles("Admin","Owner"), (req, res) => DocController.GetdocumetnById(req, res))
 
-DocumentRouter.post("/doc/GetAllFolderTree", (req, res) => DocController.GetAllFolderTree(req, res))
+DocumentRouter.post("/doc/GetAllFolderTree", authenticate, authorizeRoles("Admin","Owner"),(req, res) => DocController.GetAllFolderTree(req, res))
 
-DocumentRouter.put("/doc/UpdateFolderOrFileName", (req, res) => DocController.UpdateFolder_or_file_name(req, res))
+DocumentRouter.put("/doc/UpdateFolderOrFileName", authenticate, authorizeRoles("Admin","Owner"),(req, res) => DocController.UpdateFolder_or_file_name(req, res))
 
-DocumentRouter.delete("/doc/DeleteDocument/:folderId", (req, res) => DocController.DeletedocumentById(req, res))
+DocumentRouter.delete("/doc/DeleteDocument/:folderId", authenticate, authorizeRoles("Admin","Owner"),(req, res) => DocController.DeletedocumentById(req, res))
 
 
 
