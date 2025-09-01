85ed5fffc2a9c1f680705e5e6d7e4924f41a48d6
teklu abayneh <tekluabayneh@gmail.com>
2025-09-01 16:10:37 +0300

    feat: add create folder modal component

--- frontend/src/components/ui/CreateFolderModal.tsx
+++ frontend/src/components/ui/CreateFolderModal.tsx
@@ -1,42 +1,44 @@
-"use client"
+"use client";
 import React from "react";
-import {Node } from "../../types/Node"
-import { useFileSystem } from "@/context/FileTreeContext";
+import { Node } from "../../types/Node";
+import { useFileTree } from "@/context/EditorContext";
+
 type PropsType = {
-    setFileTree: React.Dispatch<React.SetStateAction<Node[]>>,
-    setisModalopen: React.Dispatch<React.SetStateAction<boolean>>,
-    isFile:boolean
-}
+  setFileTree: React.Dispatch<React.SetStateAction<Node[]>>;
+  setisModalopen: React.Dispatch<React.SetStateAction<boolean>>;
+  isFile: boolean;
+};
 
 const NameInputModal = () => {
-const { name, setName,handleSubmit, handleCancel} = useFileSystem()
-return (
-  <div className="mx-auto fixed inset-0 bg-opacity-20 flex items-center justify-center z-50">
-    <div className="bg-gray-900 rounded-xl shadow-2xl p-6 w-80 border border-gray-700">
-      <h2 className="text-lg font-semibold mb-4 text-white">Enter Title</h2>
-      <input
-        type="text"
-        value={name}
-        placeholder="New name..."
-        onChange={(e) => setName(e.target.value)}
-        className="w-full bg-gray-800 text-white border border-gray-600 rounded-md px-3 py-2 mb-4 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      <div className="flex justify-end gap-2">
-        <button
-          onClick={handleSubmit}
-          className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
-        >
-          Create
-        </button>
-        <button
-          onClick={handleCancel}
-          className="bg-gray-700 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition"
-        >
-          Cancel
-        </button>
+  const { name, setName, handleSubmit, handleCancel } = useFileTree();
+  return (
+    <div className="mx-auto fixed inset-0 bg-opacity-20 flex items-center justify-center z-50">
+      <div className="bg-gray-900 rounded-xl shadow-2xl p-6 w-80 border border-gray-700">
+        <h2 className="text-lg font-semibold mb-4 text-white">Enter Title</h2>
+        <input
+          type="text"
+          value={name}
+          placeholder="New name..."
+          onChange={(e) => setName(e.target.value)}
+          className="w-full bg-gray-800 text-white border border-gray-600 rounded-md px-3 py-2 mb-4 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <div className="flex justify-end gap-2">
+          <button
+            onClick={() => handleSubmit()}
+            className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
+          >
+            Create
+          </button>
+          <button
+            onClick={() => handleCancel()}
+            className="bg-gray-700 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition"
+          >
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-)}
+  );
+};
 
 export default NameInputModal;
