647ea302cc115bc4a9f7dde01d80838037f04605
teklu abayneh <tekluabayneh@gmail.com>
2025-09-03 09:15:52 +0300

    refactor: update FileTreeContext handling

--- frontend/src/context/FileTreeContext.tsx
+++ frontend/src/context/FileTreeContext.tsx
@@ -1,98 +1,57 @@
-"use client"
-import React, {createContext, ReactNode, useContext, useState , useEffect} from "react";
+"use client";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
 import { toast } from "react-hot-toast";
-import { Node , FileSystemContextType } from "@/types/Node";
+import { Node, FileSystemContextType } from "@/types/Node";
 import axios from "axios";
 const FileSystemContext = createContext<FileSystemContextType | null>(null);
 
-export const  FileSystemProvider =({children}:{children:ReactNode}) => {
+export const FileSystemProvider = ({ children }: { children: ReactNode }) => {
+  const [fileTree, setFileTree] = useState<Node[]>();
+  const email = "fake@gmail.com";
 
-    const [name, setName] = useState("");
-    const [isModabolen, setisModalopen] = useState(false)
-    const [isFile, setisFile] = useState(false)
-    const [fileTree, setFileTree] = useState<Node[]>();
-    const email = "fake@gmail.com" 
+  useEffect(() => {
+    const handleFetchFileTree = async (email: string) => {
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/api/doc/GetAllFolderTree",
+          {
+            email: email,
+          },
+          { withCredentials: true }
+        );
+        setFileTree(response.data);
+        console.log(fileTree);
+      } catch (err) {
+        console.error("Error fetching file tree:", err);
+      }
+    };
+    handleFetchFileTree(email);
+  }, [email]);
 
-useEffect(() => {
-  const handleFetchFileTree = async (email: string) => {
-  try {
-    const response = await axios.post("http://localhost:5000/api/doc/GetAllFolderTree", {
-      email: email},{ withCredentials: true});
-     setFileTree(response.data)
-    console.log(fileTree);
-  } catch (err) {
-    console.error("Error fetching file tree:", err);
-  }
+  useEffect(() => {
+    console.log("✅ File Tree Updated", JSON.stringify(fileTree, null, 2));
+  }, [fileTree]);
 
-}
-handleFetchFileTree(email)
-},[email])
-
-
-
-useEffect(() => {
-  console.log("✅ File Tree Updated", JSON.stringify(fileTree, null, 2));
-}, [fileTree]) ;
-
-    const handleSubmit = () => {
-            if(name == ""){
-                toast("Please enter a folder or file name.", {
-                    icon: "⚠️",
-                    style: {
-                        background: "#fef3c7",
-                        color: "#92400e",
-                    },
-                });
-                return 
-            }
-    }
-
-
-
-
-    const Createfolder = () =>{
-        setisModalopen(true)
-    }
-
-    const handleCancel= () => {
-        setisModalopen(false)
-    }
-
-    const  CreateFile_and_FolderWithin = (isFile:boolean) => {
-        setisModalopen(true)
-        setisFile(isFile)
-        const folderId = localStorage.getItem("folderId")
-        console.log("this",folderId)
-    }
-
-    const    Dletefile_and_folder = () => {}
-
-    return (
-        <FileSystemContext.Provider
-            value={{
-                name,
-                setName,
-                isModabolen,
-                setisModalopen,
-                isFile,
-                setisFile,
-                fileTree,
-                Createfolder ,
-                setFileTree,
-                handleSubmit,
-                handleCancel,
-                CreateFile_and_FolderWithin,
-                Dletefile_and_folder
-            }}
-        >
-            {children}
-        </FileSystemContext.Provider> 
-    )
-
-}
-export const useFileSystem = () =>{ 
-    const context = useContext(FileSystemContext)
-    if(!context) throw new Error("useFileSystem must be used within a FileSystemProvider");
-    return context
-
-}
+  return (
+    <FileSystemContext.Provider
+      value={{
+        fileTree,
+        setFileTree,
+      }}
+    >
+      {children}
+    </FileSystemContext.Provider>
+  );
+};
+export const useFileSystem = () => {
+  const context = useContext(FileSystemContext);
+  if (!context)
+    throw new Error("useFileSystem must be used within a FileSystemProvider");
+  return context;
+};
