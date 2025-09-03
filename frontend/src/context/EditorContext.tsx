a15db1bd69295cdd47d1b612fabc19c7aac44c4a
teklu abayneh <tekluabayneh@gmail.com>
2025-09-03 09:15:52 +0300

    refactor: clean up EditorContext logic

--- frontend/src/context/EditorContext.tsx
+++ frontend/src/context/EditorContext.tsx
@@ -1,5 +1,11 @@
 "use client";
-import React, { createContext, ReactNode, useState, useContext } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useState,
+  useContext,
+  useEffect,
+} from "react";
 import { DocumentType, FileSyncHandeleContentType } from "../types/document";
 import axios from "axios";
 const FileTreeContent = createContext<FileSyncHandeleContentType | null>(null);
@@ -11,23 +17,66 @@ export const FileContentProvider = ({ children }: { children: ReactNode }) => {
   const [CurrentFileInEditor, SetCurrentFileInEditor] = useState<
     DocumentType[] | null
   >(null);
+  type nodes = {
+    node: DocumentType | [];
+    name: string;
+  };
+  const [toBeUpdated, settoBeupdated] = useState<nodes>({
+    node: [],
+    name: "",
+  });
 
   const DeleteFile = () => {};
   const DeleteFolder = () => {};
 
-  const UpdateFileName_and_folderName = async (node: DocumentType) => {
+  const UpdateFileName = async (node: DocumentType) => {
     console.log("thsi is the node from the context to be updaed man", node);
     // const res = await axios.put("http://localhost:5000/api/doc/UpdateFolderOrFileName", {}, {withCredentials:true})
     // console.log(res)
+    settoBeupdated((prev) => ({
+      ...prev,
+      node: node,
+    }));
+    setIsModalOpen(true);
   };
+  const UpdateDocument = async (node: DocumentType) => {
+    console.log("thsi is the node from the context to be updaed man", node);
+    // const res = await axios.put("http://localhost:5000/api/doc/UpdateFolderOrFileName", {}, {withCredentials:true})
+    // console.log(res)
 
+    settoBeupdated((prev) => ({
+      ...prev,
+      node: node,
+    }));
+
+    setIsModalOpen(true);
+  };
   const updateFileToEditor = (file: DocumentType[]) => {
     SetCurrentFileInEditor(file);
   };
 
+  const newDocument = () => {};
+  
   const SaveFileContentToDb = () => {};
+
+  const Createfolder = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const CreateFile_and_FolderWithin = () => {
+    setIsModalOpen(true);
+    setisFile(isFile);
+  };
+
+  const Dletefile_and_folder = () => {
+    setIsModalOpen(true);
+  };
+
   const handleSubmit = () => {
-    /// if therer is not folder id in the localStorage create the file and also folder in the root level
     if (name == "") {
       toast("Please enter a folder or file name.", {
         icon: "⚠️",
@@ -38,26 +87,37 @@ export const FileContentProvider = ({ children }: { children: ReactNode }) => {
       });
       return;
     }
-    console.log("this is the name we want man", name);
+
+    settoBeupdated((prev) => ({
+      ...prev,
+      name: name,
+    }));
+    const flag = localStorage.getItem("flag");
+    function setToBeUpdatedCase() {
+      if (!flag) return;
+      switch (flag) {
+        case "newFile":
+          return "newfile man";
+        case "createFolder":
+          return "new folder man";
+        case "Rename":
+          return "renaming man";
+        case "Delete":
+          return "delete";
+        default:
+          return "nothing man";
+      }
+    }
+  useEffect(() => {
+    console.log("🔥 toBeUpdated changed:", toBeUpdated);
+  }, [toBeUpdated]);
+    const result = setToBeUpdatedCase();
+    console.log(result);
+
     setIsModalOpen(false);
+    setName("");
   };
 
-  const Createfolder = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-
-  const CreateFile_and_FolderWithin = (isFile: boolean) => {
-    setIsModalOpen(true);
-    setisFile(isFile);
-    const folderId = localStorage.getItem("folderId");
-    console.log("this", folderId);
-  };
-
-  const Dletefile_and_folder = () => {};
   return (
     <FileTreeContent.Provider
       value={{
@@ -67,10 +127,12 @@ export const FileContentProvider = ({ children }: { children: ReactNode }) => {
         CreateFile_and_FolderWithin,
         isModalOpen,
         setIsModalOpen,
-        UpdateFileName_and_folderName,
+        UpdateFileName,
         CurrentFileInEditor,
         DeleteFile,
         DeleteFolder,
+        UpdateDocument,
+        Dletefile_and_folder,
         updateFileToEditor,
         SaveFileContentToDb,
         handleSubmit,
