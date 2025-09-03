2052b8a990e6c464cb265244a63eeab464354fb2
teklu abayneh <tekluabayneh@gmail.com>
2025-09-03 09:15:52 +0300

    fix: correct behavior in CreateFolderModal

--- frontend/src/components/ui/CreateFolderModal.tsx
+++ frontend/src/components/ui/CreateFolderModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { Node } from "../../types/Node";
 import { useFileTree } from "@/context/EditorContext";
 
