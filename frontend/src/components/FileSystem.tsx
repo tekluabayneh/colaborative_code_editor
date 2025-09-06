7e3c36a8a58ff7e664c2f6e168fa45012f2facec
teklu abayneh <tekluabayneh@gmail.com>
2025-09-06 23:30:16 +0300

    fix: resolve FileSystem component issues

--- frontend/src/components/FileSystem.tsx
+++ frontend/src/components/FileSystem.tsx
@@ -4,7 +4,8 @@ import { Node } from "@/types/Node";
 export default function Page({ folders }: { folders: Node[] }) {
   return (
     <ul>
-      {folders?.map((node: Node, index: number) => (
+      {folders?.map((node, index: number) => (
+        // @ts-ignore
         <FilesystemItem node={node} key={index} />
       ))}
     </ul>
