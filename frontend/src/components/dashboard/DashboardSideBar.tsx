f363fb2b0b64d5654c4fdd4e5c0e8022ec7ecbf5
teklu abayneh <tekluabayneh@gmail.com>
2025-09-08 23:19:19 +0300

    feat: enhance DashboardSideBar component UI

--- frontend/src/components/dashboard/DashboardSideBar.tsx
+++ frontend/src/components/dashboard/DashboardSideBar.tsx
@@ -1,40 +1,43 @@
-import { BarChart3, FileText, Users, Activity, Folder, GitBranch, Code2, Home } from "lucide-react";
+import { FileText, Users, Activity, Folder, Code2, Home } from "lucide-react";
 import { Button } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
-
+import Link from "next/link";
 const navigation = [
-    { name: "Dashboard", icon: Home, current: true },
-    { name: "Projects", icon: Folder, current: false },
-    { name: "Code Editor", icon: Code2, current: false },
-    { name: "Team", icon: Users, current: false },
-    { name: "Activity", icon: Activity, current: false },
-    { name: "Analytics", icon: BarChart3, current: false },
-    { name: "Files", icon: FileText, current: false },
-    { name: "Branches", icon: GitBranch, current: false },
+  { name: "Dashboard", icon: Home, current: true, link: "/dashboard" },
+  { name: "Code Editor", icon: Code2, current: false, link: "/editor" },
+  { name: "Team", icon: Users, current: false, link: "/admin" },
+  { name: "Activity", icon: Activity, current: false, link: "/activity" },
+  { name: "Files", icon: FileText, current: false, link: "/files" },
 ];
 
 export const DashboardSidebar = () => {
-    return (
-        <div className="w-13 md:hover:w-40 transition-all  delay-200  bg-neutral-900 border-r border-gray-700 flex flex-col overflow-hidden">
-            <div className="p-4 pl-1">
-                <nav className="space-y-3 ">
-                    {navigation.map((item) => {
-                        const Icon = item.icon;
-                        return (
-                            <Button
-                                key={item.name}
-                                variant={item.current ? "secondary" : "ghost"}
-                                className={cn( " cursor-pointer justify-start gap-4 h-11", item.current && "bg-purple-400 border border-gray-700")} >
-                                <Icon className="w-5 h-5" /> 
-                                <span className="hidden md:block">
-                                    {item.name}
-                                </span>
-                            </Button>
-                        );
-                    })}
-                </nav>
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="w-13 md:hover:w-40 transition-all  delay-200  bg-neutral-900 border-r border-gray-700 flex flex-col overflow-hidden">
+      <div className="p-4 pl-1">
+        <nav className="space-y-3 ">
+          {navigation.map((item) => {
+            const Icon = item.icon;
+            return (
+              <Button
+                key={item.name}
+                variant={item.current ? "secondary" : "ghost"}
+                className={cn(
+                  " cursor-pointer h-11",
+                  item.current && "bg-purple-400 border border-gray-700"
+                )}
+              >
+                <Link
+                  href={item.link}
+                  className="flex items-center justify-center gap-4"
+                >
+                  <Icon className="w-5 h-5" />
+                  <span className="hidden md:block">{item.name}</span>
+                </Link>
+              </Button>
+            );
+          })}
+        </nav>
+      </div>
+    </div>
+  );
 };
