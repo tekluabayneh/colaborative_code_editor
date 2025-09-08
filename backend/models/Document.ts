f0ca854b0c009c48cbec627cf3ab2f38dbd6c07c
teklu abayneh <tekluabayneh@gmail.com>
2025-09-08 23:19:13 +0300

    refactor: update Document model structure

--- backend/models/Document.ts
+++ backend/models/Document.ts
@@ -3,17 +3,23 @@ import mongoose, { Schema, Document, Types } from "mongoose";
 interface IDocument extends Document {
   content: string;
   language?: string;
-  ownerType: "User" | "Owner";            
-  ownerId: Types.ObjectId;                
+  ownerType: "User" | "Owner";
+  ownerId: Types.ObjectId;
 }
 
-const DocumentSchema: Schema<IDocument> = new Schema({
-  content: { type: String },
-  language: { type: String, required:true, default: "plaintext" },
-  ownerType: { type: String, required: true, enum: ["User", "Owner"] },
-  ownerId: { type: Schema.Types.ObjectId, required: true, refPath: "ownerType" },
-});
+const DocumentSchema: Schema<IDocument> = new Schema(
+  {
+    content: { type: String },
+    language: { type: String, required: true, default: "plaintext" },
+    ownerType: { type: String, required: true, enum: ["User", "Owner"] },
+    ownerId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      refPath: "ownerType",
+    },
+  },
+  { timestamps: true }
+);
 
 const Documents = mongoose.model("Document", DocumentSchema);
 export default Documents;
-
