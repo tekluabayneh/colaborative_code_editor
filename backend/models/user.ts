9fba9e8d5fd4662a0f1a020db7c1f6d3068eb6fa
teklu abayneh <tekluabayneh@gmail.com>
2025-09-13 14:05:37 +0300

    chore: update user model

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,36 +1,35 @@
-import mongoose , {Schema }  from "mongoose";
-const userSchema:Schema = new mongoose.Schema({
-    userName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-    role: {
-        type: String,
-        enum: ["Editor", "Admin", "Reviewer", "Read_only"],
-    },
-    invitedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Onwser",
-        required: true
-    },
-   createdAt:{ 
-	type:Date,
-	}
-
-})
+import mongoose, { Schema } from "mongoose";
+const userSchema: Schema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["Editor", "Admin", "Reviewer", "Read_only"],
+  },
+  invitedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Owner",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+  },
+});
 
 const Users = mongoose.model("User", userSchema);
 
