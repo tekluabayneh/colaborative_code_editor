31b2592bb80248446dc93bf1782e58302fc71686
teklu abayneh <tekluabayneh@gmail.com>
2025-09-17 11:59:22 +0300

    refactor: update auth routes

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,20 +1,22 @@
 import express from "express";
 import authController from "../controllers/auth.controller";
 import AuthMiddleware from "../middlewares/auth.middleware";
-const { Login, Register , ResetPassword, verifyOtp, SendOTP, sendResetLink } = authController;
-const { ResgisterValidate, LoginValidate ,ResetPasswordValidate } = AuthMiddleware;
+const { Login, Register, ResetPassword, verifyOtp, SendOTP, sendResetLink } =
+  authController;
+const { ResgisterValidate, LoginValidate, ResetPasswordValidate } =
+  AuthMiddleware;
 const AuthRouter = express.Router();
 
 AuthRouter.post("/auth/login", LoginValidate, Login);
 
 AuthRouter.post("/auth/register", ResgisterValidate, Register);
 
-AuthRouter.post("/auth/sendOtp", SendOTP )
+AuthRouter.post("/auth/sendOtp", SendOTP);
 
-AuthRouter.post("/auth/verifyOtp", verifyOtp)
+AuthRouter.post("/auth/verifyOtp", verifyOtp);
 
-AuthRouter.post("/auth/sendRestLink", sendResetLink)
+AuthRouter.post("/auth/sendRestLink", sendResetLink);
 
-AuthRouter.post("/auth/ResetPassword", ResetPasswordValidate, ResetPassword) 
+AuthRouter.post("/auth/ResetPassword", ResetPasswordValidate, ResetPassword);
 
 export default AuthRouter;
