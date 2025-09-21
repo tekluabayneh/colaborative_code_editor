541b584c60c10641a19bb97c3b5718e03a8b81a0
teklu abayneh <tekluabayneh@gmail.com>
2025-09-21 22:03:49 +0300

    fix: adjust role middleware

--- backend/middlewares/role.middleware.ts
+++ backend/middlewares/role.middleware.ts
@@ -1,18 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 
 const authorizeRoles = (...roles: string[]) => {
-
   return (req: Request, res: Response, next: NextFunction) => {
     try {
-      if (!req.token || !req.token.role) { 
+      if (!req.token || !req.token.role) {
         res.status(401).json({ message: "Not authenticated" });
         return;
       }
 
       const token = req.token;
-      console.log("log from authorizeRoles", token);
 
-      const hasRole = roles.some(r => r.toLowerCase() === token.role.toLowerCase());
+      const hasRole = roles.some(
+        (r) => r.toLowerCase() === token.role.toLowerCase()
+      );
 
       if (!hasRole) {
         res.status(403).json({ message: "Forbidden: insufficient role" });
@@ -20,12 +20,10 @@ const authorizeRoles = (...roles: string[]) => {
       }
 
       next();
-    } catch (error) { 
+    } catch (error) {
       res.status(500).json({ message: "internal server error", error });
-      console.log("error here ma", error);
     }
   };
 };
 
 export default authorizeRoles;
-
