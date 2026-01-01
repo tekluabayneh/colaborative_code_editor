7ccfd0ec66fd56ec1a046000fb32cea54e389221
Teklu <tekluabayneh@gmail.com>
2026-01-01 15:19:41 +0900

    refactor(backend): update build file tree utility

--- backend/Utils/BuildFileTree.ts
+++ backend/Utils/BuildFileTree.ts
@@ -1,18 +1,18 @@
-type FileList ={
-  name:string,
-  type:string,
-  _id:string,
-  parentId:number,
-    }
+// type FileList ={
+//   name:string,
+//   type:string,
+//   _id:string,
+//   parentId:number,
+//     }
 
-const BuildTree =(FlatList:FileList , parentId:string) => {
+// const BuildTree =(FlatList:FileList , parentId:string) => {
 
-return <FileList>FlatList.filter((item) => String(item.parentId) == String(parentId))
-    .map(item =>(
-     {...item,
-        children: BuildTree(FlatList, item._id)
-        }
-        ))
-}
+// return <FileList>FlatList.filter((item) => String(item.parentId) == String(parentId))
+//     .map(item =>(
+//      {...item,
+//         children: BuildTree(FlatList, item._id)
+//         }
+//         ))
+// }
 
-export default BuildTree
+// export default BuildTree
