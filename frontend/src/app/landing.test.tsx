df371f261c1e9f83af66f402ecf9475a4ece5f21
Teklu <tekluabayneh@gmail.com>
2026-01-02 15:02:12 +0900

    Update landing page test

--- frontend/src/app/landing.test.tsx
+++ frontend/src/app/landing.test.tsx
@@ -2,63 +2,62 @@ import "@testing-library/jest-dom";
 import Landing from "./page";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import * as nextNavigation  from "next/navigation"
-jest.mock("next/navigation", () => ({ 
- useRouter: jest.fn() 
- }))
+jest.mock("next/navigation", () => ({
+    useRouter: jest.fn()
+}))
 
-describe("check if the landing page is rendeinng the component propely", () => { 
-  it("check if component are render", () =>{ 
-    const {container} = render(<Landing/>);
-    expect(container).toMatchSnapshot()
-  })
+describe("check if the landing page is rendeinng the component propely", () => {
+    it("check if component are render", () => {
+        const { container } = render(<Landing />);
+        expect(container).toMatchSnapshot()
+    })
 
 
-	it("assert buttons for auth", () => { 
-             render(<Landing/>);
-         const RegButton = screen.getByRole("button", {name:/Register Now/i})
-         const SignButton = screen.getByRole("button", {name:/Sign in/i})
-         const StartButton = screen.getByRole("button", {name:/Start today for free/i})
+    it("assert buttons for auth", () => {
+        render(<Landing />);
+        const RegButton = screen.getByRole("button", { name: /Register Now/i })
+        const SignButton = screen.getByRole("button", { name: /Sign in/i })
+        const StartButton = screen.getByRole("button", { name: /Start today for free/i })
 
-         expect(RegButton).toBeInTheDocument()          
-         expect(SignButton).toBeInTheDocument()          
-         expect(StartButton).toBeInTheDocument()          
+        expect(RegButton).toBeInTheDocument()
+        expect(SignButton).toBeInTheDocument()
+        expect(StartButton).toBeInTheDocument()
 
-	})
+    })
 
-	it("sign in button should navigate to Auth url", async () => { 
-         	 const push:jest.Mock<void, [string]> = jest.fn()
-             render(<Landing/>);
-            const RegButton = screen.getByRole("button", {name:/Register Now/i})
+    it("sign in button should navigate to Auth url", async () => {
+        const push: jest.Mock<void, [string]> = jest.fn()
+        render(<Landing />);
+        const RegButton = screen.getByRole("button", { name: /Register Now/i })
 
-              await userEvent.click(RegButton)	
-              expect(push).toHaveBeenCalledWith("/Auth") 
-	})
+        await userEvent.click(RegButton)
+        expect(push).toHaveBeenCalledWith("/Auth")
+    })
 
-     it.only("should toggle the editor perview image", async () => { 
-             render(<Landing/>);
-	const EditorBtn  = screen.getByRole("button", {name:/code editor/i}) 
-	const CustomEditorBtn  = screen.getByRole("button", {name:/custom editor/i}) 
+    it.only("should toggle the editor perview image", async () => {
+        render(<Landing />);
+        const EditorBtn = screen.getByRole("button", { name: /code editor/i })
+        const CustomEditorBtn = screen.getByRole("button", { name: /custom editor/i })
 
-       expect(EditorBtn).toBeInTheDocument()
-       expect(CustomEditorBtn).toBeInTheDocument()
+        expect(EditorBtn).toBeInTheDocument()
+        expect(CustomEditorBtn).toBeInTheDocument()
 
-        await userEvent.hover(EditorBtn) 
+        await userEvent.hover(EditorBtn)
         expect(EditorBtn).toHaveClass("cursor-pointer")
- 
-        await userEvent.hover(CustomEditorBtn) 
+
+        await userEvent.hover(CustomEditorBtn)
         expect(EditorBtn).toHaveClass("cursor-pointer")
-                 
-       await userEvent.click(EditorBtn) 
-	expect(EditorBtn).toHaveClass("bg-white text-black shadow-lg shadow-white/20") 
+
+        await userEvent.click(EditorBtn)
+        expect(EditorBtn).toHaveClass("bg-white text-black shadow-lg shadow-white/20")
 
 
-       await userEvent.click(CustomEditorBtn) 
-	expect(CustomEditorBtn).toHaveClass("bg-gradient-to-r from-purple-600 to-pink-600 text-white shadow-lg shadow-purple-600/30") 
+        await userEvent.click(CustomEditorBtn)
+        expect(CustomEditorBtn).toHaveClass("bg-gradient-to-r from-purple-600 to-pink-600 text-white shadow-lg shadow-purple-600/30")
 
 
-     })
-      
+    })
 
-}) 
+
+})
 
