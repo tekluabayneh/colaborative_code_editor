cd1eb7c44cccb330bc87402b1a89431e85622d27
Teklu <tekluabayneh@gmail.com>
2026-01-02 15:02:12 +0900

    Update FileSystem component

--- frontend/src/components/FileSystem.tsx
+++ frontend/src/components/FileSystem.tsx
@@ -2,12 +2,12 @@ import { FilesystemItem } from "./filesystem-item";
 import { Node } from "@/types/Node";
 
 export default function Page({ folders }: { folders: Node[] }) {
-  return (
-    <ul>
-      {folders?.map((node, index: number) => (
-        // @ts-ignore
-        <FilesystemItem node={node} key={index} />
-      ))}
-    </ul>
-  );
+    return (
+        <ul>
+            {folders?.map((node, index: number) => (
+                // @ts-expect-error node tyeps is spesifyed
+                <FilesystemItem node={node} key={index} />
+            ))}
+        </ul>
+    );
 }
