850f422d3cd6c9e0dff2a2353daf44a470b1db2f
Teklu <tekluabayneh@gmail.com>
2026-01-02 15:02:12 +0900

    Update FAQ component

--- frontend/src/components/FAQ.tsx
+++ frontend/src/components/FAQ.tsx
@@ -4,7 +4,7 @@ import { faqItems } from '@/data/FQA_data';
 const FAQ = () => {
     const [openItems, setOpenItems] = useState([0]);
 
-    const toggleItem = (index:number) => {
+    const toggleItem = (index: number) => {
         setOpenItems(prev =>
             prev.includes(index)
                 ? prev.filter(i => i !== index)
@@ -22,7 +22,7 @@ const FAQ = () => {
                     </h2>
                 </div>
                 <p className="text-lg text-gray-400">
-                    Have questions? We have answers. If you can't find what you're looking for, feel free to{' '}
+                    Have questions? We have answers. If you cant find what youre looking for, feel free to{' '}
                     <a href="#" className="font-medium bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent hover:brightness-125 transition">
                         contact us
                     </a>.
@@ -31,21 +31,20 @@ const FAQ = () => {
 
             <div className="space-y-4">
                 {faqItems.map((item, index) => (
-                    <div 
-                        key={index} 
+                    <div
+                        key={index}
                         className="bg-white/5 border border-white/10 rounded-xl backdrop-blur-sm transition-all duration-300 hover:bg-white/10" >
                         <button
                             onClick={() => toggleItem(index)}
-			    data-testid={`testid_${index}`}
+                            data-testid={`testid_${index}`}
                             className="w-full py-5 px-6 flex justify-between items-center text-left gap-4"
                         >
                             <span className="text-lg font-medium text-white">{item.question}</span>
                             <div className="w-7 h-7 flex-shrink-0 flex items-center justify-center bg-white/10 rounded-full">
-                                <Plus 
-				   data-testid={`plus_btn_${index}`}
-                                    className={`w-5 h-5 text-gray-300 transition-transform cursor-pointer duration-300 ${
-                                        openItems.includes(index) ? 'rotate-45' : 'rotate-0'
-                                    }`} 
+                                <Plus
+                                    data-testid={`plus_btn_${index}`}
+                                    className={`w-5 h-5 text-gray-300 transition-transform cursor-pointer duration-300 ${openItems.includes(index) ? 'rotate-45' : 'rotate-0'
+                                        }`}
                                 />
                             </div>
                         </button>
