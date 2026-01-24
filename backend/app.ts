55ae3eb04df8aa7566b48d60ca0f35dd5e9e1b13
Teklu <tekluabayneh@gmail.com>
2026-01-24 21:08:04 +0900

    update CORS

--- backend/app.ts
+++ backend/app.ts
@@ -9,7 +9,7 @@ import OAuthRouter from "./routes/OAuth.route";
 import passport from "passport"
 import session from "express-session"
 import inviteRouter from "./routes/invite.route";
-import {configuregoogleAuth, configuregithubstrateg } from "./controllers/outh.controller";
+import { configuregoogleAuth, configuregithubstrateg } from "./controllers/outh.controller";
 import DocumentRouter from "./routes/document.route";
 import Owners from "./models/Owners";
 import GloblaError from "./middlewares/error.middleware"
@@ -17,31 +17,31 @@ import cookieParser from "cookie-parser";
 import UserRouter from "./routes/user.route";
 import AICodeCompletionRouter from "./routes/codeCompletion.route";
 
- 
-  
+
+
 const app = express();
 
-type user ={
-	_id?:number
+type user = {
+    _id?: number
 }
 
 app.use(
-  cors({
-    origin: "http://localhost:3000", 
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials:true,
-  })
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
 );
 
 
 app.use(
-  session({
-    secret: "my-secret",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
+    session({
+        secret: "my-secret",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    })
 );
 
 
@@ -56,40 +56,40 @@ configuregithubstrateg(passport);
 configuregoogleAuth(passport);
 
 
-passport.serializeUser((user:user, done) => {
-  done(null, user._id ?? "1")
+passport.serializeUser((user: user, done) => {
+    done(null, user._id ?? "1")
 });
 
 
 passport.deserializeUser(async (id, done) => {
-  try {
-const findUser = await Owners.findOne({id:id})	
+    try {
+        const findUser = await Owners.findOne({ id: id })
 
- if(!findUser) return  done(null, false)
-      return done(null, findUser) 
-  } catch (err) {
-    return done(err);
-  }
+        if (!findUser) return done(null, false)
+        return done(null, findUser)
+    } catch (err) {
+        return done(err);
+    }
 });
 
 
 app.use(helmet());
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 const RateLimit = rateLimit({
-  windowMs: 5 * 60 * 1000, 
-  max: 10, 
-  message: "Too many requests, please try again later.",
+    windowMs: 5 * 60 * 1000,
+    max: 10,
+    message: "Too many requests, please try again later.",
 });
 
 // app.use(RateLimit);
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  const header = req.headers.authorization;
-  console.log("Authorization Header:", header);
-  res.send("Hello from TypeScript + Express!");
+    const header = req.headers.authorization;
+    console.log("Authorization Header:", header);
+    res.send("Hello from TypeScript + Express!");
 });
 
 
@@ -100,14 +100,14 @@ app.use("/api", AuthRouter);
 app.use("/api", OAuthRouter);
 
 // invite user 
-app.use("/api",  inviteRouter);
+app.use("/api", inviteRouter);
 
 
 // document route 
-app.use("/api",DocumentRouter)
+app.use("/api", DocumentRouter)
 
 // User Route
-app.use("/api",UserRouter)
+app.use("/api", UserRouter)
 
 // get the ai code completion
 app.use("/api", AICodeCompletionRouter);
