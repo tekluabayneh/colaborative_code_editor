9162a9420f9badfea2e53ac1ef52d5469b0193e6
Teklu <tekluabayneh@gmail.com>
2026-01-28 00:26:09 +0900

    chore(editor): update editor layout

--- frontend/src/app/editor/layout.tsx
+++ frontend/src/app/editor/layout.tsx
@@ -1,18 +1,20 @@
 import type { Metadata } from "next";
-import {FileContentProvider} from "../../context/EditorContext"
-
+import { FileContentProvider } from "../../context/EditorContext"
+import { EnvFileSystem } from "../../context/getNextConfigEnv"
 export const metadata: Metadata = {
-  title: "editor page",
-  description: "editor apge",
+    title: "editor page",
+    description: "editor apge",
 };
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
-  return (
-      <div> 
-        <FileContentProvider>
-		{children}
-        </FileContentProvider>
-      </div>
-  );
+    return (
+        <div>
+            <FileContentProvider>
+                <EnvFileSystem>
+                    {children}
+                </ EnvFileSystem>
+            </FileContentProvider>
+        </div>
+    );
 }
 
