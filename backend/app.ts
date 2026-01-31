dd6df93b0b1541a2822100b4eff8e72a324072d6
Teklu <tekluabayneh@gmail.com>
2026-02-01 06:22:49 +0900

    update cors

--- backend/app.ts
+++ backend/app.ts
@@ -25,40 +25,40 @@ type user = {
     _id?: number
 }
 
-app.use(cors({
-    origin: true,             // allows any origin that requests (very permissive in dev)
-    credentials: true         // if you use cookies / auth headers later
-}));
+// app.use(cors({
+//     origin: true,             
+//     credentials: true        
+// }));
 
-//
-// app.use((req, res, next) => {
-//     const origin = req.headers.origin;
-//
-//     if (origin && origin === process.env.ORIGIN_FRONTEND_URL) {
-//         res.setHeader("Access-Control-Allow-Origin", origin);
-//         res.setHeader("Vary", "Origin");
-//     }
-//
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//
-//     res.setHeader(
-//         "Access-Control-Allow-Methods",
-//         "GET, POST, PUT, DELETE, OPTIONS"
-//     );
-//
-//     if (req.method === "OPTIONS") {
-//         res.sendStatus(204);
-//         return
-//     }
-//
-//     next();
-// });
-//
-// app.set("trust proxy", 1);
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+
+    if (origin && origin === process.env.ORIGIN_FRONTEND_URL) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Vary", "Origin");
+    }
+
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+
+    if (req.method === "OPTIONS") {
+        res.sendStatus(204);
+        return
+    }
+
+    next();
+});
+
+app.set("trust proxy", 1);
 
 app.use(
     session({
